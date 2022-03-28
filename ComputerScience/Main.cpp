f7d0ebf5432d41e277aba02f4ac2dbaf6323cce3
unknown <asionneau@artfx.fr>
2022-03-28 14:40:38 +0200

    factorial2

---
+++ ComputerScience/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num = 0;
+    cout << "Enter a number : ";
+    cin >> num;
+    cout << "Factorial of entered number: " << factorial(num);
+    return 0;
+}
